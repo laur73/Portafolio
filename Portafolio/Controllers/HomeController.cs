967ed992cacea8d37261daa6ff4a582497cc478e
laur73 <jj.c.r.1781@gmail.com>
2022-03-17 13:24:44 -0600

    Se agregó la descripción, habilidades y vistas parciales

--- Portafolio/Controllers/HomeController.cs
+++ Portafolio/Controllers/HomeController.cs
@@ -15,11 +15,41 @@ namespace Portafolio.Controllers
 
         public IActionResult Index()
         {
-            //Declaramos un ViewBag.Propiedad = Valor
-            ViewBag.Nombre = "Marco Pestaña";
-            ViewBag.Edad = 22;
+            return View();
+        }
 
-            return View("Index");
+        private List<ProyectoViewModel> ObtenerProyectos()
+        {
+            return new List<ProyectoViewModel>() {
+                new ProyectoViewModel
+            {
+                Titulo = "Amazon",
+                Descripcion ="E-Commerce realizado en ASP.NET Core",
+                Link = "https://amazon.com",
+                ImagenURL = ""
+            },
+                  new ProyectoViewModel
+            {
+                Titulo = "New York Times",
+                Descripcion ="Página de noticias en React",
+                Link = "https://nytimes.com",
+                ImagenURL = ""
+            },
+                    new ProyectoViewModel
+            {
+                Titulo = "Reddit",
+                Descripcion ="Red social para compartir en comunidades",
+                Link = "https://reddit.com",
+                ImagenURL = ""
+            },
+                      new ProyectoViewModel
+            {
+                Titulo = "Steam",
+                Descripcion ="Tienda en línea para comprar videojuegos",
+                Link = "https://store.steampowered.com",
+                ImagenURL = ""
+            }
+            };
         }
 
         public IActionResult Privacy()
