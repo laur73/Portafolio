d38670c01596b996556ab99269465495b0948bfd
laur73 <jj.c.r.1781@gmail.com>
2022-03-18 10:38:50 -0600

    Se instanció el RepositorioProyecto

--- Portafolio/Controllers/HomeController.cs
+++ Portafolio/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Portafolio.Models;
+using Portafolio.Services;
 using System.Diagnostics;
 
 namespace Portafolio.Controllers
@@ -15,44 +16,13 @@ namespace Portafolio.Controllers
 
         public IActionResult Index()
         {
-            var proyectos = ObtenerProyectos().Take(3).ToList();
+            var repositorioProyectos = new RepositorioProyectos();
+            var proyectos = repositorioProyectos.ObtenerProyectos().Take(3).ToList();
             var modelo = new HomeIndexViewModel() { Proyectos = proyectos };
             return View(modelo);
         }
 
-        private List<ProyectoViewModel> ObtenerProyectos()
-        {
-            return new List<ProyectoViewModel>() {
-                new ProyectoViewModel
-            {
-                Titulo = "Amazon",
-                Descripcion ="E-Commerce realizado en ASP.NET Core",
-                Link = "https://amazon.com",
-                ImagenURL = "/imagenes/amazon.png"
-            },
-                  new ProyectoViewModel
-            {
-                Titulo = "New York Times",
-                Descripcion ="Página de noticias en React",
-                Link = "https://nytimes.com",
-                ImagenURL = "/imagenes/nyt.png"
-            },
-                    new ProyectoViewModel
-            {
-                Titulo = "Reddit",
-                Descripcion ="Red social para compartir en comunidades",
-                Link = "https://reddit.com",
-                ImagenURL = "/imagenes/reddit.png"
-            },
-                      new ProyectoViewModel
-            {
-                Titulo = "Steam",
-                Descripcion ="Tienda en línea para comprar videojuegos",
-                Link = "https://store.steampowered.com",
-                ImagenURL = "/imagenes/steam.png"
-            }
-            };
-        }
+        
 
         public IActionResult Privacy()
         {
